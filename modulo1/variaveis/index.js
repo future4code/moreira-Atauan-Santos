56135aca2f9825f76ddaff5de49277eb15537641
Atauan LIma <94688135+Atauan@users.noreply.github.com>
2021-11-29 16:30:23 -0300

    Atividade de Variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,41 @@
+
+// Atividade de escrita.
+/* // Questão 1
+
+const nome = prompt("Digite Seu Nome")
+let idade = prompt("Digite sua idade")
+console.log("nome", typeof nome)
+console.log("idade", typeof idade)
+
+//Ele me informou que embos são string 
+
+console.log("Olá", nome,  "você tem", idade, "anos")
+
+ */
+// Questão 2
+/* const VoceEstuda = "- SIM"
+const NaoEstuda = "- NÃO"
+const VoceJaAlmoçou = "- SIM"
+
+
+console.log("Você já estudou Hoje?" , VoceEstuda,".")
+console.log("Você Se não estudou Hoje?" , NaoEstuda,".")
+console.log("Você Se já almoçou Hoje?" , VoceJaAlmoçou,".") */
+
+
+//Criei 3 novas variáveis, contendo as respostas ao lado das suas perguntas.
+
+/* // Questão 3 
+let a = 10
+let b = 25
+
+/* Aqui faremos uma lógica para trocar os valores seria adicionar um novo copo vazio e jogar A na C então
+joguei o A no B já que o B está vazio troco pelo C que tem o A. */
+
+/* let c = a;
+a = b;
+b = c;
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10 */
