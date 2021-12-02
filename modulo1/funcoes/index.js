599b0906247a8da02594a337f8a0192172fbe2a6
Atauan LIma <94688135+Atauan@users.noreply.github.com>
2021-12-02 16:47:56 -0300

    Atividade Aula Funções

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,79 @@
+// Questão 1
+
+/* function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+minhaFuncao(2)
+minhaFuncao(10)
+
+// A - Ele vai imprime 10 e 50.
+
+// B - Não imprime nada.
+
+// Questão 2
+
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+    
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+
+// A - A função pede para verificar se você escreve cenoura ele te devolve
+// true se for diferente de "cenoura" é false se for
+
+// B - Vai sair como true pois independente da palavra cenoura ele determina em "toLowerCase"  que independe ele vai ficar minusculo.
+// então vai trazer só true em ambas.
+
+// Exercicio Escrita
+
+// Questão 1
+function NomeIdadeMoraEstuda(nome, idade, mora, estudante) {
+    console.log('Oi, ', nome,'! Você tem ', idade, 'anos, mora ', mora, 'e sou estudante', estudante)
+}
+
+NomeIdadeMoraEstuda('Atauan', '26', 'Aracaju', 'sim' ) */
+
+/* // Questão 2
+
+// A) Escreva uma função que receba 2 números como parâmetros, e, 
+// dentro da função, faça a soma das duas entradas e retorne o resultado. 
+// Invoque a função e imprima no console o resultado
+
+const soma = (n1, n2) => {
+    return n1 + n2
+}
+console.log(soma(10, 20))
+
+// B) Faça uma função que recebe 2 números e retorne um 
+// booleano que informa se o primeiro número é maior ou igual ao segundo.
+
+const comparacao =(n1, n2) => {
+    return n1 >= n2
+}
+console.log(comparacao(30, 20))
+
+// C) Escreva uma função que receba um número 
+// e devolva um booleano indicando se ele é par ou não
+
+const NumeroPar =(numero) =>{
+    return numero % 2 === 0
+}
+console.log(NumeroPar(12))
+
+// D) Faça uma função que recebe uma mensagem (string) como parâmetro e imprima o 
+// tamanho dessa mensagem, juntamente com uma versão dela em letras maiúsculas.
+
+const FraseString = (textoDoUsuario) => {
+    textoDoUsuario = textoDoUsuario.toUpperCase();
+        return textoDoUsuario
+}
+
+const fraseStringPronta = FraseString("Eu quero ser desenvolvedor Pleno")
+console.log(fraseStringPronta.length, fraseStringPronta) */
+
