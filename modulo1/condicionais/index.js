d48a102579995465abc0167dd86ac0f0501256f0
Atauan LIma <94688135+Atauan@users.noreply.github.com>
2021-12-08 00:37:28 -0300

    Adição final do exercício - tarefa dois.

--- modulo1/condicionais/index.js
+++ modulo1/condicionais/index.js
@@ -97,6 +97,7 @@ let generoFilme = prompt("Qual gênero do Filme que deseja assistir?")
 /*******************************************************************************************************/
     // DESAFIO
 
+// 1
 let genero = prompt("Qual gênero do Filme que deseja assistir?")
 let preco = Number(prompt("Quanto custa o ingresso do Filme?"))
 
@@ -111,3 +112,123 @@ function verificarFilmeIngresso (genero,preco) {
 }
 
 verificarFilmeIngresso(genero,preco)
+
+/*******************************************************************************************************/
+
+// 2
+
+const nome = prompt("Digite o seu nome completo:")
+const games = prompt("Digite IN caso o games for internacional ou DO caso seja doméstico").toUpperCase()
+const etapasdojogo = prompt("Digite SF para semi-final, DT para decisão de terceiro lugar ou FI para final").toUpperCase()
+const categoriaDosJogos = Number(prompt("Qual a categoriaDosJogos? Opções: 1, 2, 3 ou 4."))
+const quantidadeIngreco = Number(prompt("Qual a quantidade de ingressos?"))
+let valorDosIngressos
+
+function calculandotudo() {
+    return valorDosIngressos * quantidadeIngreco
+}
+
+function imprimeJogos() {
+    if (games === "IN") {
+        return "Internacional"
+    } else {
+        return "Doméstico"
+    }
+}
+
+function imprimiEtapas() {
+    if (etapasdojogo === "SF") {
+        return "Semi Final"
+    } else if (etapasdojogo === "DT") {
+        return "Decisão 3º lugar"
+    } else {
+        return "Final"
+    }
+}
+
+if (categoriaDosJogos === 1 && games === "IN" && etapasdojogo === "SF") {
+    valorDosIngressos = 5412
+    calculandotudo()
+} else if (categoriaDosJogos === 2 && games === "IN" && etapasdojogo === "SF") {
+    valorDosIngressos = 3608
+    calculandotudo()
+} else if (categoriaDosJogos === 3 && games === "IN" && etapasdojogo === "SF") {
+    valorDosIngressos = 2255
+    calculandotudo()
+} else if (categoriaDosJogos === 4 && games === "IN" && etapasdojogo === "SF") {
+    valorDosIngressos = 902
+    calculandotudo()
+} else if (categoriaDosJogos === 1 && games === "IN" && etapasdojogo === "DT") {
+    valorDosIngressos = 2706
+    calculandotudo()
+} else if (categoriaDosJogos === 2 && games === "IN" && etapasdojogo === "DT") {
+    valorDosIngressos = 1804
+    calculandotudo()
+} else if (categoriaDosJogos === 3 && games === "IN" && etapasdojogo === "DT") {
+    valorDosIngressos = 1353
+    calculandotudo()
+} else if (categoriaDosJogos === 4 && games === "IN" && etapasdojogo === "DR") {
+    valorDosIngressos = 697
+    calculandotudo()
+} else if (categoriaDosJogos === 1 && games === "IN" && etapasdojogo === "FI") {
+    valorDosIngressos = 8118
+    calculandotudo()
+} else if (categoriaDosJogos === 2 && games === "IN" && etapasdojogo === "FI") {
+    valorDosIngressos = 5412
+    calculandotudo()
+} else if (categoriaDosJogos === 3 && games === "IN" && etapasdojogo === "FI") {
+    valorDosIngressos = 3608
+    calculandotudo()
+} else if (categoriaDosJogos === 4 && games === "IN" && etapasdojogo === "FI") {
+    valorDosIngressos = 1353
+    calculandotudo()
+} else if (categoriaDosJogos === 1 && games === "DO" && etapasdojogo === "SF") {
+    valorDosIngressos = 1320
+    calculandotudo()
+} else if (categoriaDosJogos === 2 && games === "DO" && etapasdojogo === "SF") {
+    valorDosIngressos = 880
+    calculandotudo()
+} else if (categoriaDosJogos === 3 && games === "DO" && etapasdojogo === "SF") {
+    valorDosIngressos = 550
+    calculandotudo()
+} else if (categoriaDosJogos === 4 && games === "DO" && etapasdojogo === "SF") {
+    valorDosIngressos = 220
+    calculandotudo()
+} else if (categoriaDosJogos === 1 && games === "DO" && etapasdojogo === "DT") {
+    valorDosIngressos = 660
+    calculandotudo()
+} else if (categoriaDosJogos === 2 && games === "DO" && etapasdojogo === "DT") {
+    valorDosIngressos = 440
+    calculandotudo()
+} else if (categoriaDosJogos === 3 && games === "DO" && etapasdojogo === "DT") {
+    valorDosIngressos = 330
+    calculandotudo()
+} else if (categoriaDosJogos === 4 && games === "DO" && etapasdojogo === "DT") {
+    valorDosIngressos = 170
+    calculandotudo()
+} else if (categoriaDosJogos === 1 && games === "DO" && etapasdojogo === "FI") {
+    valorDosIngressos = 1980
+    calculandotudo()
+} else if (categoriaDosJogos === 2 && games === "DO" && etapasdojogo === "FI") {
+    valorDosIngressos = 1320
+    calculandotudo()
+} else if (categoriaDosJogos === 3 && games === "DO" && etapasdojogo === "FI") {
+    valorDosIngressos = 880
+    calculandotudo()
+} else if (categoriaDosJogos === 4 && games === "DO" && etapasdojogo === "FI") {
+    valorDosIngressos = 330
+    calculandotudo()
+}
+
+console.log(`---Dados da compra--- 
+Nome do cliente:  ${nome} 
+Tipo do games:  ${imprimeJogos()} 
+Etapa do games:  ${imprimiEtapas()} 
+Categoria:  ${categoriaDosJogos} 
+Quantidade de Ingressos:  ${quantidadeIngreco} 
+---Valores--- 
+Valor do ingresso:  R$ ${valorDosIngressos}
+Valor total:  ${calculandotudo()}`)
+
+
+
